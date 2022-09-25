587dfd03a8488e7aaf15cbfd08a99565bc8c8673
Neeraj <78613839+Chinni566@users.noreply.github.com>
2022-09-25 13:33:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+img = "";
+
+status = "";
+objects = [];
+
+
+function setup(){
+    canvas = createCanvas( 380 , 380);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML  =  "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model is loaded");
+     status = true;
+
+   objectDetector.detect(img , gotResults);
+}
+
+function gotResults(error , results){
+    if (error) {
+       console.log(error);
+
+    } 
+    else {
+       console.log(results);
+       objects = results;
+    }
+}
